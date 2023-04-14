bbe12d0fd08d04558f84b23366e215df57fed31b
bbode27 <89547668+bbode27@users.noreply.github.com>
2023-04-14 13:09:54 -0500

    PUT IT ON THE DAMN RADIO

---
+++ bin/firebase_testing.js
@@ -0,0 +1,61 @@
+import firebase from "firebase/compat/app";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://support.google.com/firebase/answer/7015592
+const firebaseConfig = {
+    apiKey: "AIzaSyDwIMuoA52lc04e5-nLWgflKw2nwO1pe3E",
+    authDomain: "athena-e6a1b.firebaseapp.com",
+    databaseURL: "https://athena-e6a1b-default-rtdb.firebaseio.com",
+    projectId: "athena-e6a1b",
+    storageBucket: "athena-e6a1b.appspot.com",
+    messagingSenderId: "393710869045",
+    appId: "1:393710869045:web:8efe705365a228d92faf7c",
+    measurementId: "G-427F8MHZPF"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+//console.log(auth)
+
+const db = getFirestore(app);
+
+
+import { collection , getDocs } from "firebase/firestore";
+
+const querySnapshot = await getDocs(collection(db, "Question"));
+console.log("Got snapshot")
+querySnapshot.forEach((doc) => {
+  console.log(`${doc.id} => ${doc.data().QuestionText}`);
+});
+
+// const docRef = doc(db, "Questions", "RUWn9A6We8LG27N34yo7");
+// const docSnap = await getDoc(docRef);
+
+// if (docSnap.exists()) {
+//   console.log("Document data:", docSnap.data());
+// } else {
+//   // docSnap.data() will be undefined in this case
+//   console.log("No such document!");
+// }
+
+import { Server } from "socket.io";
+
+
+const io = new Server(80, {
+  // options
+});
+
+io.on("connection", (socket) => {
+    console.log("connected to 80");
+    socket.on("join", () => {
+        console.log("received from front end");
+    });
+});
+
+console.log("bottom of file");
+
+
